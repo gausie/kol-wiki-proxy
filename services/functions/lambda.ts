e053a6aa5c9fa529e03981be886184a6f508bfd7
Sam Gaus <sam@gaus.co.uk>
2022-08-26 22:30:05 +0100

    Fix up TS and make content type flow through

--- services/functions/lambda.ts
+++ services/functions/lambda.ts
@@ -2,10 +2,12 @@ import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import fetch from "node-fetch";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  const { Host, host, Origin, origin, ...headers } = event.headers || {};
+  const headers = Object.fromEntries(Object.entries(event.headers || {})
+    .filter(([k, v]) => !["host", "origin"].includes(k.toLowerCase()) && v !== undefined)
+    .map(i => i as [string, string]));
 
   const requestParams = Object.entries(event.queryStringParameters || {})
-    .reduce((acc, param) => [...acc, param.join("=")], [])
+    .reduce((acc, param) => [...acc, param.join("=")], [] as string[])
     .join('&');
 
   const url = `https://kol.coldfront.net/thekolwiki/api.php?${requestParams}`;
@@ -20,14 +22,14 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
         'Access-Control-Allow-Origin': '*',
         // Required for cookies, authorization headers with HTTPS
         'Access-Control-Allow-Credentials': true,
-        'content-type': res.headers['content-type'],
+        'content-type': res.headers.get("content-type") || "text/plain",
       },
       body,
     };
-  } catch (err) {
+  } catch (error) {
     return {
       statusCode: 500,
-      body: err.name,
+      body: (error as Error).name,
     };;
   }
 };
